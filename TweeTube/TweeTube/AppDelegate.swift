2627f23b45c477793600dd094acfb4edbab2d9fd
Héctor Gonzalo Andrés <hectorgoan@Hectors-MacBook-Pro.local>
2023-02-02 19:58:01 +0100

    First Commit

---
+++ TweeTube/TweeTube/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TweeTube
+//
+//  Created by Héctor Gonzalo Andrés on 02/02/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TweeTube/TweeTube/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TweeTube
+//
+//  Created by Héctor Gonzalo Andrés on 02/02/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
